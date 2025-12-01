fb5e9cde79229164419c1ad7d11706ec3b52c83b
manideepgrandhi <manideepgrandhi@hotmail.com>
2025-12-01 17:39:02 +0530

    Replace M3FDBGRD with dbxyz in 00_whoisactive/nutreco_noted.sql

--- 00_whoisactive/nutreco_noted.sql
+++ 00_whoisactive/nutreco_noted.sql
@@ -1,34 +1,34 @@
-> Around 10:45 PM CEST, please change the AUTO_UPDATE_STATISTICS_ASYNC to OFF for database M3FDBGRD.
-
-USE [master]
-GO
-ALTER DATABASE [M3FDBGRD] SET AUTO_UPDATE_STATISTICS_ASYNC OFF WITH NO_WAIT
-GO
-
-> Start the "Blockings_capture" extended event session.
-
-ALTER EVENT SESSION Blockings_Capture ON SERVER STATE = START;
-
-
->Reindex job starts at 11PM CEST,Adjusted the index settings table to execute maintenance on "MITPLO" table first.
-Use the below query to check the status of reindexing.Around 12 indexes are there on the table(1 Clustered,11 non-clustered)
-
-
-SELECT  *
-  FROM [dba].[Minion].[IndexMaintLogDetails]
-  where [ExecutionDateTime] >'2020-09-18 23:00:00.837'
-  and TableName='MITPLO'
---and FragLevel between 10 and 20
-and Status='Complete'
-order by OpBeginDateTime desc
-
->Once reindex process completes on MITPLO,Change the AUTO_UPDATE_STATISTICS_ASYNC to ON
-
-USE [master]
-GO
-ALTER DATABASE [M3FDBGRD] SET AUTO_UPDATE_STATISTICS_ASYNC ON WITH NO_WAIT
-GO
-
-> Start the "Blockings_capture" extended event session.
-
-ALTER EVENT SESSION Blockings_capture ON SERVER STATE = STOP;
+1| > Around 10:45 PM CEST, please change the AUTO_UPDATE_STATISTICS_ASYNC to OFF for database dbxyz.
+2| 
+3| USE [master]
+4| GO
+5| ALTER DATABASE [dbxyz] SET AUTO_UPDATE_STATISTICS_ASYNC OFF WITH NO_WAIT
+6| GO
+7| 
+8| > Start the "Blockings_capture" extended event session.
+9| 
+10| ALTER EVENT SESSION Blockings_Capture ON SERVER STATE = START;
+11| 
+12| 
+13| >Reindex job starts at 11PM CEST,Adjusted the index settings table to execute maintenance on "MITPLO" table first.
+14| Use the below query to check the status of reindexing.Around 12 indexes are there on the table(1 Clustered,11 non-clustered)
+15| 
+16| 
+17| SELECT  *
+18|   FROM [dba].[Minion].[IndexMaintLogDetails]
+19|   where [ExecutionDateTime] >'2020-09-18 23:00:00.837'
+20|   and TableName='MITPLO'
+21| --and FragLevel between 10 and 20
+22| and Status='Complete'
+23| order by OpBeginDateTime desc
+24| 
+25| >Once reindex process completes on MITPLO,Change the AUTO_UPDATE_STATISTICS_ASYNC to ON
+26| 
+27| USE [master]
+28| GO
+29| ALTER DATABASE [dbxyz] SET AUTO_UPDATE_STATISTICS_ASYNC ON WITH NO_WAIT
+30| GO
+31| 
+32| > Start the "Blockings_capture" extended event session.
+33| 
+34| ALTER EVENT SESSION Blockings_capture ON SERVER STATE = STOP;
